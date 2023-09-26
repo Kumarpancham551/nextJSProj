6abe2135034d03e9bdf3bceeb6f1446ab6a3811e
Pancham <pamcham@diamondsoncall.com>
2023-09-26 19:02:32 +0530

    show task page done

--- src/app/show-tasks/ShowTasks.jsx
+++ src/app/show-tasks/ShowTasks.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+"use client"
+import UserContext from '@/context/userContext'
+import { getTaskOfUser } from '@/services/taskService'
+import React, { useContext, useEffect, useState } from 'react'
+import Task from './Task'
 
 const ShowTasks = () => {
+  const [tasks, setTasks] = useState([])
+
+  const context = useContext(UserContext)
+
+  async function loadTasks(userID) {
+    try {
+      const tasks = await getTaskOfUser(userID);
+      setTasks([...tasks].reverse());
+      console.log(tasks)
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+
+  useEffect(() => {
+    if(context.user){
+      loadTasks(context.user._id);
+    }
+  }, [context.user])
   return (
-    <div>ShowTasks</div>
+    <div className='grid grid-cols-12 mt-3'>
+      <div className='col-span-6 col-start-4'>
+          <h1 className='text-3xl mb-3'>Your tasks {tasks.length}</h1>
+
+          {
+            tasks.map((task)=>(
+              <Task task={task} key={task._id}/>
+            ))
+          }
+      </div>
+    </div>
   )
 }
 
